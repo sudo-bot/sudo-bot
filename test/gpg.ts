fee51521a75763d74cd7302593039970b5374e74
William Desportes <williamdes@wdes.fr>
2022-12-21 10:58:08 +0400

    tests: fix gpg sign test

--- test/gpg.ts
+++ test/gpg.ts
@@ -6,18 +6,19 @@ import { suite, before } from 'mocha';
 
 suite('gpg', function () {
     test('testSign', function (done) {
-        const testMessage = '';
+        const testMessage = 'testok';
         gpg.signCommit(testMessage, {
             file: __dirname + '/data/sudo-bot-test.priv',
             passphrase: 'fYWwRmbkcd3tXSqcCrjWBCFBhzEW6ASP98zcSH',
         })
             .then((signature) => {
+                expect(signature).to.include(
+                    '-----BEGIN PGP SIGNED MESSAGE-----\nHash: SHA512\n\ntestok\n-----BEGIN PGP SIGNATURE-----\n\n'
+                );
                 expect(signature).to.include('-----BEGIN PGP SIGNATURE-----\n\n');
                 expect(signature).to.include('\n-----END PGP SIGNATURE-----\n');
                 done();
             })
-            .catch((err) => {
-                throw err;
-            });
+            .catch((err) => done(err));
     }).timeout('10s');
 });
